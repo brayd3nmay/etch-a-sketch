0be65af21c0ba890848ab79a4895f863f8bf8489
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-06-16 20:04:22 -0500

    Fix makeGrid to make the correct number of divs
    Originally the function was making a num amount of divs. The fix was in the for loop of the function.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// appends num * num amount of block class divs to the container
 function makeGrid(num) {
     // reset the current grid
     const container = document.querySelector('#container');
     container.innerHTML = '';
 
     // make num amount of blocks for grid
-    for(let i = 0; i < num; i++) {
+    for(let i = 0; i < num * num; i++) {
         const block = document.createElement('div');
         block.className = 'block';
 
